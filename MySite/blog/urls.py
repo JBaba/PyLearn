dfc2137ac71e275f92ab8b59ba36821b7e63c086
JBaba <naimish101.viradia@gmail.com>
2017-01-20 15:16:24 -0500

    blog model template done

--- MySite/blog/urls.py
+++ MySite/blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
 """ . means here import from current packages """
 from . import views
 
 urlpatterns = [
-    url(r'^blog/', views.blog, name='blog'),
+    url(r'^blog/', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],
+    	template_name="blog/home.html")),
 ]
